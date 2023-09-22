5ed7ada360b0be9d48b88b46a8ab88b17d9a2c3b
BertVerhees <bert.verhees@rosa.nl>
2023-09-22 17:23:10 +0200

    progress

--- docs/html_parser_bmm.go
+++ docs/html_parser_bmm.go
@@ -11,13 +11,12 @@ import (
 )
 
 func main() {
-	fileName := "index.html"
+	fileName := "bmm.html"
 	text, err := readHtmlFromFile(fileName)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	data := parse(text)
 	fmt.Println(data)
 }
@@ -30,12 +29,10 @@ func readHtmlFromFile(fileName string) (string, error) {
 	return string(bs), nil
 }
 
-func parse(text string) (data []string) {
+func parse(text string) (data string) {
 	tkn := html.NewTokenizer(strings.NewReader(text))
 
-	var vals []string
-
-	var isLi bool
+	var h4 bool
 
 	for {
 
@@ -44,22 +41,34 @@ func parse(text string) (data []string) {
 		switch {
 
 		case tt == html.ErrorToken:
-			return vals
+			return ""
 
 		case tt == html.StartTagToken:
 
 			t := tkn.Token()
-			isLi = t.Data == "li"
-
-		case tt == html.TextToken:
-
-			t := tkn.Token()
-
-			if isLi {
-				vals = append(vals, t.Data)
+			h4 = t.Data == "h4"
+			if h4 {
+				fmt.Println("->" + t.Data)
+				tt = tkn.Next()
+				t := tkn.Token()
+				a := t.Data == "a"
+				if a {
+					fmt.Println("---->" + t.Data)
+					tt = tkn.Next()
+					tt = tkn.Next()
+					t3 := tkn.Token()
+					span := t3.Data == "span"
+					if span {
+						fmt.Println("---------->" + t3.Data)
+						tt = tkn.Next()
+						t4 := tkn.Token()
+						if tt == html.TextToken {
+							fmt.Println("---------->" + t4.Data)
+						}
+					}
+				}
+				h4 = false
 			}
-
-			isLi = false
 		}
 	}
 }
