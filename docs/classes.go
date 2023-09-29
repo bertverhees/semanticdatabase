abef89681a1a117f0184f71d8725f266b13a1920
BertVerhees <bert.verhees@rosa.nl>
2023-09-29 20:21:53 +0200

    class/file structure progress

--- docs/classes.go
+++ docs/classes.go
@@ -1,10 +1,27 @@
 package main
 
+import "strings"
+
 type Class struct {
 	Comment    string
-	Inherits   []Class
+	Inherits   []string
 	Attributes []Attribute
 	Functions  []Function
+}
+
+func NewClass(comment, inherits string) (*Class, error) {
+	var class *Class
+	class = new(Class)
+	class.Comment = comment
+	//inherits-slice always exists
+	if inherits != "" {
+		class.Inherits = strings.Split(inherits, ",")
+	} else {
+		class.Inherits = make([]string, 0)
+	}
+	class.Attributes = make([]Attribute, 0)
+	class.Functions = make([]Function, 0)
+	return class, nil
 }
 
 type Constant struct {
@@ -13,10 +30,28 @@ type Constant struct {
 	Comment string
 }
 
+func NewConstant(name, _type, comment string) (*Constant, error) {
+	var constant *Constant
+	constant.Name = name
+	constant.Type = _type
+	constant.Comment = comment
+	return constant, nil
+}
+
 type Attribute struct {
-	Name    string
-	Type    string
-	Comment string
+	Name     string
+	Type     string
+	Comment  string
+	Required bool
+}
+
+func NewAttribute(name, _type, comment string, required bool) (*Attribute, error) {
+	var attribute *Attribute
+	attribute.Name = name
+	attribute.Type = _type
+	attribute.Comment = comment
+	attribute.Required = required
+	return attribute, nil
 }
 
 type Function struct {
@@ -26,7 +61,27 @@ type Function struct {
 	Comment string
 }
 
+func NewFunction(name, comment string) (*Function, error) {
+	var function *Function
+	function.Name = name
+	function.Comment = comment
+	function.Out = make([]Parameter, 0)
+	function.In = make([]Parameter, 0)
+	return function, nil
+}
+
 type Parameter struct {
-	in_out  string
-	Comment string
+	Name     string
+	In_out   string
+	Type     string
+	Required bool
+}
+
+func NewParameter(name, in_out, _type string, required bool) (*Parameter, error) {
+	var parameter *Parameter
+	parameter.Name = name
+	parameter.In_out = name
+	parameter.Type = _type
+	parameter.Required = required
+	return parameter, nil
 }
