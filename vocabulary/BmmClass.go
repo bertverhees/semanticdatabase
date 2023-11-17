df61951d5b0c5a79dce76177b85bb86ae735f3ef
BertVerhees <bert.verhees@rosa.nl>
2023-11-17 14:09:04 +0100

    renaming attributes, creating getters/setters, integrating error-handling

--- vocabulary/BmmClass.go
+++ vocabulary/BmmClass.go
@@ -14,6 +14,35 @@ type IBmmClass interface {
 	IBmmModule
 	//BMM_CLASS
 	//redefined in subclasses _type() IBmmModelType
+	Package() IBmmPackage
+	SetPackage(_package IBmmPackage) error
+	Properties() map[string]IBmmProperty
+	SetProperties(properties map[string]IBmmProperty) error
+	SourceSchemaId() string
+	SetSourceSchemaId(sourceSchemaId string) error
+	ImmediateDescendants() []IBmmClass
+	SetImmediateDescendants(immediateDescendants []IBmmClass) error
+	IsOverride() bool
+	SetIsOverride(isOverride bool) error
+	StaticProperties() map[string]IBmmStatic
+	SetStaticProperties(staticProperties map[string]IBmmStatic) error
+	Functions() map[string]IBmmFunction
+	SetFunctions(functions map[string]IBmmFunction) error
+	Procedures() map[string]IBmmProcedure
+	SetProcedures(procedures map[string]IBmmProcedure) error
+	IsAbstract() bool
+	SetIsAbstract(isAbstract bool) error
+	Invariants() []IBmmAssertion
+	SetInvariants(invariants []IBmmAssertion) error
+	Creators() map[string]IBmmProcedure
+	SetCreators(creators map[string]IBmmProcedure) error
+	Converters() map[string]IBmmProcedure
+	SetConverters(converters map[string]IBmmProcedure) error
+	Ancestors() map[string]IBmmModelType
+	SetAncestors(ancestors map[string]IBmmModelType) error
+	IsPrimitive() bool
+	SetIsPrimitive(isPrimitive bool) error
+
 	AllAncestors() []string
 	AllDescendants() []string
 	Suppliers() []string
@@ -23,7 +52,6 @@ type IBmmClass interface {
 	ClassPath() string
 	FlatFeatures()
 	FlatProperties() []IBmmProperty
-	IsPrimitive() bool
 }
 
 // Struct definition
@@ -32,33 +60,33 @@ type BmmClass struct {
 	// Constants
 	// Attributes
 	// List of immediate inheritance parents.
-	Ancestors map[string]IBmmModelType `yaml:"ancestors" json:"ancestors" xml:"ancestors"`
+	ancestors map[string]IBmmModelType `yaml:"ancestors" json:"ancestors" xml:"ancestors"`
 	// Package this class belongs to.
-	Package IBmmPackage `yaml:"package" json:"package" xml:"package"`
+	_package IBmmPackage `yaml:"package" json:"package" xml:"package"`
 	// Properties defined in this class (subset of features ).
-	Properties map[string]IBmmProperty `yaml:"properties" json:"properties" xml:"properties"`
+	properties map[string]IBmmProperty `yaml:"properties" json:"properties" xml:"properties"`
 	/**
 	Reference to original source schema defining this class. Useful for UI tools to
 	determine which original schema file to open for a given class for manual
 	editing.
 	*/
-	SourceSchemaId string `yaml:"sourceschemaid" json:"sourceschemaid" xml:"sourceschemaid"`
+	sourceSchemaId string `yaml:"sourceschemaid" json:"sourceschemaid" xml:"sourceschemaid"`
 	/**
 	List of computed references to base classes of immediate inheritance
 	descendants, derived when members of ancestors are attached at creation time.
 	*/
-	ImmediateDescendants []IBmmClass `yaml:"immediatedescendants" json:"immediatedescendants" xml:"immediatedescendants"`
+	immediateDescendants []IBmmClass `yaml:"immediatedescendants" json:"immediatedescendants" xml:"immediatedescendants"`
 	/**
 	True if this definition overrides a class of the same name in an included
 	schema.
 	*/
-	IsOverride bool `yaml:"isoverride" json:"isoverride" xml:"isoverride"`
+	isOverride bool `yaml:"isoverride" json:"isoverride" xml:"isoverride"`
 	// Static properties defined in this class (subset of features ).
-	StaticProperties map[string]IBmmStatic `yaml:"staticproperties" json:"staticproperties" xml:"staticproperties"`
+	staticProperties map[string]IBmmStatic `yaml:"staticproperties" json:"staticproperties" xml:"staticproperties"`
 	// Functions defined in this class (subset of features ).
-	Functions map[string]IBmmFunction `yaml:"functions" json:"functions" xml:"functions"`
+	functions map[string]IBmmFunction `yaml:"functions" json:"functions" xml:"functions"`
 	// Procedures defined in this class (subset of features ).
-	Procedures map[string]IBmmProcedure `yaml:"procedures" json:"procedures" xml:"procedures"`
+	procedures map[string]IBmmProcedure `yaml:"procedures" json:"procedures" xml:"procedures"`
 	/**
 	True if this class represents a type considered to be primitive in the type
 	system, i.e. any typically built-in or standard library type such as String ,
@@ -69,20 +97,137 @@ type BmmClass struct {
 	True if this class is marked as abstract, i.e. direct instances cannot be
 	created from its direct type.
 	*/
-	IsAbstract bool            `yaml:"isabstract" json:"isabstract" xml:"isabstract"`
-	Invariants []IBmmAssertion `yaml:"invariants" json:"invariants" xml:"invariants"`
+	isAbstract bool            `yaml:"isabstract" json:"isabstract" xml:"isabstract"`
+	invariants []IBmmAssertion `yaml:"invariants" json:"invariants" xml:"invariants"`
 	/**
 	Subset of procedures that may be used to initialise a new instance of an object,
 	and whose execution will guarantee that class invariants are satisfied.
 	*/
-	Creators map[string]IBmmProcedure `yaml:"creators" json:"creators" xml:"creators"`
+	creators map[string]IBmmProcedure `yaml:"creators" json:"creators" xml:"creators"`
 	/**
 	Subset of creators that create a new instance from a single argument of another
 	type.
 	*/
-	Converters map[string]IBmmProcedure `yaml:"converters" json:"converters" xml:"converters"`
+	converters map[string]IBmmProcedure `yaml:"converters" json:"converters" xml:"converters"`
 	// features of this module.
 	features []IBmmFeature `yaml:"features" json:"features" xml:"features"` //redefined
+}
+
+func (b *BmmClass) Package() IBmmPackage {
+	return b._package
+}
+
+func (b *BmmClass) SetPackage(_package IBmmPackage) error {
+	b._package = _package
+	return nil
+}
+
+func (b *BmmClass) Properties() map[string]IBmmProperty {
+	return b.properties
+}
+
+func (b *BmmClass) SetProperties(properties map[string]IBmmProperty) error {
+	b.properties = properties
+	return nil
+}
+
+func (b *BmmClass) SourceSchemaId() string {
+	return b.sourceSchemaId
+}
+
+func (b *BmmClass) SetSourceSchemaId(sourceSchemaId string) error {
+	b.sourceSchemaId = sourceSchemaId
+	return nil
+}
+
+func (b *BmmClass) ImmediateDescendants() []IBmmClass {
+	return b.immediateDescendants
+}
+
+func (b *BmmClass) SetImmediateDescendants(immediateDescendants []IBmmClass) error {
+	b.immediateDescendants = immediateDescendants
+	return nil
+}
+
+func (b *BmmClass) IsOverride() bool {
+	return b.isOverride
+}
+
+func (b *BmmClass) SetIsOverride(isOverride bool) error {
+	b.isOverride = isOverride
+	return nil
+}
+
+func (b *BmmClass) StaticProperties() map[string]IBmmStatic {
+	return b.staticProperties
+}
+
+func (b *BmmClass) SetStaticProperties(staticProperties map[string]IBmmStatic) error {
+	b.staticProperties = staticProperties
+	return nil
+}
+
+func (b *BmmClass) Functions() map[string]IBmmFunction {
+	return b.functions
+}
+
+func (b *BmmClass) SetFunctions(functions map[string]IBmmFunction) error {
+	b.functions = functions
+	return nil
+}
+
+func (b *BmmClass) Procedures() map[string]IBmmProcedure {
+	return b.procedures
+}
+
+func (b *BmmClass) SetProcedures(procedures map[string]IBmmProcedure) error {
+	b.procedures = procedures
+	return nil
+}
+
+func (b *BmmClass) IsAbstract() bool {
+	return b.isAbstract
+}
+
+func (b *BmmClass) SetIsAbstract(isAbstract bool) error {
+	b.isAbstract = isAbstract
+	return nil
+}
+
+func (b *BmmClass) Invariants() []IBmmAssertion {
+	return b.invariants
+}
+
+func (b *BmmClass) SetInvariants(invariants []IBmmAssertion) error {
+	b.invariants = invariants
+	return nil
+}
+
+func (b *BmmClass) Creators() map[string]IBmmProcedure {
+	return b.creators
+}
+
+func (b *BmmClass) SetCreators(creators map[string]IBmmProcedure) error {
+	b.creators = creators
+	return nil
+}
+
+func (b *BmmClass) Converters() map[string]IBmmProcedure {
+	return b.converters
+}
+
+func (b *BmmClass) SetConverters(converters map[string]IBmmProcedure) error {
+	b.converters = converters
+	return nil
+}
+
+func (b *BmmClass) Ancestors() map[string]IBmmModelType {
+	return b.ancestors
+}
+
+func (b *BmmClass) SetAncestors(ancestors map[string]IBmmModelType) error {
+	b.ancestors = ancestors
+	return nil
 }
 
 func (b *BmmClass) IsPrimitive() bool {
