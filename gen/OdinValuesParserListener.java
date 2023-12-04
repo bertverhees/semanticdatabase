f856d2f30da31d4d3e5df1ffd8b8ff9b6e27cee4
BertVerhees <bert.verhees@rosa.nl>
2023-12-04 15:21:33 +0100

    removed the v2 package, and brought the files together because of not allowed cyclic references between packages in golang

---
+++ gen/OdinValuesParserListener.java
@@ -0,0 +1,379 @@
+// Generated from /home/verhees/Development/SemanticDatabase/antlr_source/bmm/OdinValuesParser.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeListener;
+
+/**
+ * This interface defines a complete listener for a parse tree produced by
+ * {@link OdinValuesParser}.
+ */
+public interface OdinValuesParserListener extends ParseTreeListener {
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#primitiveObject}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrimitiveObject(OdinValuesParser.PrimitiveObjectContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#primitiveObject}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrimitiveObject(OdinValuesParser.PrimitiveObjectContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#primitiveValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrimitiveValue(OdinValuesParser.PrimitiveValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#primitiveValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrimitiveValue(OdinValuesParser.PrimitiveValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#primitiveListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrimitiveListValue(OdinValuesParser.PrimitiveListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#primitiveListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrimitiveListValue(OdinValuesParser.PrimitiveListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#primitiveIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterPrimitiveIntervalValue(OdinValuesParser.PrimitiveIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#primitiveIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitPrimitiveIntervalValue(OdinValuesParser.PrimitiveIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#stringValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterStringValue(OdinValuesParser.StringValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#stringValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitStringValue(OdinValuesParser.StringValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#stringListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterStringListValue(OdinValuesParser.StringListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#stringListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitStringListValue(OdinValuesParser.StringListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#integerValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterIntegerValue(OdinValuesParser.IntegerValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#integerValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitIntegerValue(OdinValuesParser.IntegerValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#integerListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterIntegerListValue(OdinValuesParser.IntegerListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#integerListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitIntegerListValue(OdinValuesParser.IntegerListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#integerIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterIntegerIntervalValue(OdinValuesParser.IntegerIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#integerIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitIntegerIntervalValue(OdinValuesParser.IntegerIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#integerIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterIntegerIntervalListValue(OdinValuesParser.IntegerIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#integerIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitIntegerIntervalListValue(OdinValuesParser.IntegerIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#realValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterRealValue(OdinValuesParser.RealValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#realValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitRealValue(OdinValuesParser.RealValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#realListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterRealListValue(OdinValuesParser.RealListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#realListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitRealListValue(OdinValuesParser.RealListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#realIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterRealIntervalValue(OdinValuesParser.RealIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#realIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitRealIntervalValue(OdinValuesParser.RealIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#realIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterRealIntervalListValue(OdinValuesParser.RealIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#realIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitRealIntervalListValue(OdinValuesParser.RealIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#booleanValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterBooleanValue(OdinValuesParser.BooleanValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#booleanValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitBooleanValue(OdinValuesParser.BooleanValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#booleanListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterBooleanListValue(OdinValuesParser.BooleanListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#booleanListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitBooleanListValue(OdinValuesParser.BooleanListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#characterValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterCharacterValue(OdinValuesParser.CharacterValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#characterValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitCharacterValue(OdinValuesParser.CharacterValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#characterListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterCharacterListValue(OdinValuesParser.CharacterListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#characterListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitCharacterListValue(OdinValuesParser.CharacterListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateValue(OdinValuesParser.DateValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateValue(OdinValuesParser.DateValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateListValue(OdinValuesParser.DateListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateListValue(OdinValuesParser.DateListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateIntervalValue(OdinValuesParser.DateIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateIntervalValue(OdinValuesParser.DateIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateIntervalListValue(OdinValuesParser.DateIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateIntervalListValue(OdinValuesParser.DateIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#timeValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTimeValue(OdinValuesParser.TimeValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#timeValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTimeValue(OdinValuesParser.TimeValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#timeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTimeListValue(OdinValuesParser.TimeListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#timeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTimeListValue(OdinValuesParser.TimeListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#timeIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTimeIntervalValue(OdinValuesParser.TimeIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#timeIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTimeIntervalValue(OdinValuesParser.TimeIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#timeIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTimeIntervalListValue(OdinValuesParser.TimeIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#timeIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTimeIntervalListValue(OdinValuesParser.TimeIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateTimeValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateTimeValue(OdinValuesParser.DateTimeValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateTimeValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateTimeValue(OdinValuesParser.DateTimeValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateTimeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateTimeListValue(OdinValuesParser.DateTimeListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateTimeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateTimeListValue(OdinValuesParser.DateTimeListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateTimeIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateTimeIntervalValue(OdinValuesParser.DateTimeIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateTimeIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateTimeIntervalValue(OdinValuesParser.DateTimeIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#dateTimeIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDateTimeIntervalListValue(OdinValuesParser.DateTimeIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#dateTimeIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDateTimeIntervalListValue(OdinValuesParser.DateTimeIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#durationValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDurationValue(OdinValuesParser.DurationValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#durationValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDurationValue(OdinValuesParser.DurationValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#durationListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDurationListValue(OdinValuesParser.DurationListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#durationListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDurationListValue(OdinValuesParser.DurationListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#durationIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDurationIntervalValue(OdinValuesParser.DurationIntervalValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#durationIntervalValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDurationIntervalValue(OdinValuesParser.DurationIntervalValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#durationIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterDurationIntervalListValue(OdinValuesParser.DurationIntervalListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#durationIntervalListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitDurationIntervalListValue(OdinValuesParser.DurationIntervalListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#termCodeValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermCodeValue(OdinValuesParser.TermCodeValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#termCodeValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermCodeValue(OdinValuesParser.TermCodeValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#termCodeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void enterTermCodeListValue(OdinValuesParser.TermCodeListValueContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#termCodeListValue}.
+	 * @param ctx the parse tree
+	 */
+	void exitTermCodeListValue(OdinValuesParser.TermCodeListValueContext ctx);
+	/**
+	 * Enter a parse tree produced by {@link OdinValuesParser#relop}.
+	 * @param ctx the parse tree
+	 */
+	void enterRelop(OdinValuesParser.RelopContext ctx);
+	/**
+	 * Exit a parse tree produced by {@link OdinValuesParser#relop}.
+	 * @param ctx the parse tree
+	 */
+	void exitRelop(OdinValuesParser.RelopContext ctx);
+}
---
+++ gen/OdinValuesParserVisitor.java
@@ -0,0 +1,234 @@
+// Generated from /home/verhees/Development/SemanticDatabase/antlr_source/bmm/OdinValuesParser.g4 by ANTLR 4.13.1
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link OdinValuesParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface OdinValuesParserVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#primitiveObject}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrimitiveObject(OdinValuesParser.PrimitiveObjectContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#primitiveValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrimitiveValue(OdinValuesParser.PrimitiveValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#primitiveListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrimitiveListValue(OdinValuesParser.PrimitiveListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#primitiveIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrimitiveIntervalValue(OdinValuesParser.PrimitiveIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#stringValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStringValue(OdinValuesParser.StringValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#stringListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitStringListValue(OdinValuesParser.StringListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#integerValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIntegerValue(OdinValuesParser.IntegerValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#integerListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIntegerListValue(OdinValuesParser.IntegerListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#integerIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIntegerIntervalValue(OdinValuesParser.IntegerIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#integerIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitIntegerIntervalListValue(OdinValuesParser.IntegerIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#realValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRealValue(OdinValuesParser.RealValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#realListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRealListValue(OdinValuesParser.RealListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#realIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRealIntervalValue(OdinValuesParser.RealIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#realIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRealIntervalListValue(OdinValuesParser.RealIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#booleanValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBooleanValue(OdinValuesParser.BooleanValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#booleanListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitBooleanListValue(OdinValuesParser.BooleanListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#characterValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCharacterValue(OdinValuesParser.CharacterValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#characterListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCharacterListValue(OdinValuesParser.CharacterListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateValue(OdinValuesParser.DateValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateListValue(OdinValuesParser.DateListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateIntervalValue(OdinValuesParser.DateIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateIntervalListValue(OdinValuesParser.DateIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#timeValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTimeValue(OdinValuesParser.TimeValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#timeListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTimeListValue(OdinValuesParser.TimeListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#timeIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTimeIntervalValue(OdinValuesParser.TimeIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#timeIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTimeIntervalListValue(OdinValuesParser.TimeIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateTimeValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateTimeValue(OdinValuesParser.DateTimeValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateTimeListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateTimeListValue(OdinValuesParser.DateTimeListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateTimeIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateTimeIntervalValue(OdinValuesParser.DateTimeIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#dateTimeIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDateTimeIntervalListValue(OdinValuesParser.DateTimeIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#durationValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDurationValue(OdinValuesParser.DurationValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#durationListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDurationListValue(OdinValuesParser.DurationListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#durationIntervalValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDurationIntervalValue(OdinValuesParser.DurationIntervalValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#durationIntervalListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitDurationIntervalListValue(OdinValuesParser.DurationIntervalListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#termCodeValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermCodeValue(OdinValuesParser.TermCodeValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#termCodeListValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitTermCodeListValue(OdinValuesParser.TermCodeListValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link OdinValuesParser#relop}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRelop(OdinValuesParser.RelopContext ctx);
+}
