9dfc116c4cee5644f2afd767a7f0b634c57e9dba
BertVerhees <bert.verhees@rosa.nl>
2024-01-29 15:21:41 +0100

    primitves and identifiers

--- foundation/interval/ProperInterval.go
+++ foundation/interval/ProperInterval.go
@@ -2,6 +2,7 @@ package interval
 
 import (
 	"github.com/bertverhees/interval"
+	"semanticdatabase/foundation/primitives"
 	"semanticdatabase/generics"
 )
 
@@ -10,5 +11,24 @@ _type representing a 'proper' Interval, i.e. any two-sided or one-sided interval
 Inv_not_point: lower /= upper
 */
 type ProperInterval[T generics.Number] struct {
+	primitives.Any
 	interval.Interval[T]
+}
+
+/*Value equality: return True if this and other are attached to objects considered to be equal in value.
+ */
+func (p *ProperInterval[T]) IsEqual(b primitives.IAny) *primitives.Boolean {
+	return primitives.NewBoolean(
+		p.Lower() == b.(*ProperInterval[T]).Lower() &&
+			p.Upper() == b.(*ProperInterval[T]).Upper() &&
+			p.LowerIncluded() == b.(*ProperInterval[T]).LowerIncluded() &&
+			p.UpperIncluded() == b.(*ProperInterval[T]).UpperIncluded() &&
+			p.LowerUnbounded() == b.(*ProperInterval[T]).LowerUnbounded() &&
+			p.UpperUnbounded() == b.(*ProperInterval[T]).UpperUnbounded())
+}
+
+/*Reference equality for reference types, value equality for value types.
+ */
+func (p *ProperInterval[T]) Equal(any primitives.IAny) *primitives.Boolean {
+	return p.IsEqual(any)
 }
